1d1490a58ff031a5ed99a854d60d75b284ddfdc9
Cristelm03 <fmoralesl5@miumg.edu.gt>
2025-11-15 00:11:23 -0600

    FEAT: Repository de Factura

--- src/main/java/com/project/multipedidos_api_proveedores/repository/FacturaRepository.java
+++ src/main/java/com/project/multipedidos_api_proveedores/repository/FacturaRepository.java
@@ -3,4 +3,5 @@ package com.project.multipedidos_api_proveedores.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.project.multipedidos_api_proveedores.entity.Factura;
 
+//Repository de Factura
 public interface FacturaRepository extends JpaRepository<Factura, Long> {}
