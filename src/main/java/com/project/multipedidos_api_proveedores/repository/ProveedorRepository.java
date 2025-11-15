26272332b7e385fb691b3d16975f0bff9b77c9a3
Cristelm03 <fmoralesl5@miumg.edu.gt>
2025-11-15 00:12:08 -0600

    FEAT: Repository de Proveedores

--- src/main/java/com/project/multipedidos_api_proveedores/repository/ProveedorRepository.java
+++ src/main/java/com/project/multipedidos_api_proveedores/repository/ProveedorRepository.java
@@ -3,4 +3,5 @@ package com.project.multipedidos_api_proveedores.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.project.multipedidos_api_proveedores.entity.Proveedor;
 
+//Repository de Proveedores
 public interface ProveedorRepository extends JpaRepository<Proveedor, Long> {}
