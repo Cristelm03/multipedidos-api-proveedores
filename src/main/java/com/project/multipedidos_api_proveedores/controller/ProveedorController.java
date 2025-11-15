765ddcb9bd33c622874606b68a63e610febc4f8b
Cristelm03 <fmoralesl5@miumg.edu.gt>
2025-11-15 00:08:15 -0600

    FEAT: Creacion de Controlador de API proveedores

--- src/main/java/com/project/multipedidos_api_proveedores/controller/ProveedorController.java
+++ src/main/java/com/project/multipedidos_api_proveedores/controller/ProveedorController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+//Escucha en el puerto 8081
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api")
